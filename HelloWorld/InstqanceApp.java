47a3280333bd68807415d0c3d30bd034ab974938
umingming <u_0112@naver.com>
2021-08-15 17:52:03 +0900

    test

---
+++ HelloWorld/InstqanceApp.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+public class InstqanceApp {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintWriter p1 = new PrintWriter("result1.txt");
+		p1.write("Hello 1");
+		p1.close();
+		
+		PrintWriter p2 = new PrintWriter("result2.txt");
+		p2.write("Hello 2");
+		p2.close();
+	}
+
+}
