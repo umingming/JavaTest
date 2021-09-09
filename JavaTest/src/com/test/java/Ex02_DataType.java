164c8f781170f5cd43e35716f9541ba501b5c7ec
umingming <u_0112@naver.com>
2021-09-09 17:08:41 +0900

    변수 선언과 초기화

--- JavaTest/src/com/test/java/Ex02_DataType.java
+++ JavaTest/src/com/test/java/Ex02_DataType.java
@@ -2,4 +2,42 @@ package com.test.java;
 
 public class Ex02_DataType {
 
+	 public static void main(String[] args) {
+		 
+//		 성적 처리
+//		 1. 국어점수 조작(예측)
+//		 2. 국어 점수 저장할 공간 확보(예측)
+//		 3. 국어 점수 성질->형태+길이->정의->형태와 길이
+//		 4. 예측 결과와 가장 적합한 자바의 자료형 선택
+//		 5. 형태 선택
+//		 6. 명령어->메모리->형태에 따라 공간을 얻어오기->변수 생성
+		 
+		 byte kor;    //변수 선언
+		 kor = 10;   //데이터 대입하기
+		 System.out.println(kor);
+		 int societyScore;
+		 double pi;
+		 pi = 3.14;
+		 System.out.println(pi);
+		 
+		 byte eng;
+		 eng = 80;
+		 System.out.println(eng);  //eng 출력
+		 
+		 System.out.println(80);   //80 출력
+		 
+		 byte a = 123;
+		 System.out.println(a);
+		 a = 10;
+		 System.out.println(a);
+		 
+		 final byte score = 100;
+		 System.out.println(score);
+	
+		 
+		 
+		 
+		 
+		 
+	}
 }
