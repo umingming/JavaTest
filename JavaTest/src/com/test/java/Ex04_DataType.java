d94db0794ef9470ac424592d4a61e50732bf516f
umingming <u_0112@naver.com>
2021-09-09 18:50:05 +0900

    변수 초기화

---
+++ JavaTest/src/com/test/java/Ex04_DataType.java
@@ -0,0 +1,13 @@
+package com.test.java;
+
+public class Ex04_DataType {
+
+	public static void main(String[] args) {
+		
+		int a;		//int
+		short b;	//short
+		System.out.println("//이것은 주석이 아님");
+
+	}
+
+}
