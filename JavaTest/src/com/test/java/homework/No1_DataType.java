8afd2b08aa6b9a56b8f56e1fb3946caf88d1cc48
umingming <u_0112@naver.com>
2021-09-10 12:47:10 +0900

    short

--- JavaTest/src/com/test/java/homework/No1_DataType.java
+++ JavaTest/src/com/test/java/homework/No1_DataType.java
@@ -8,20 +8,77 @@ public class No1_DataType {
 		
 		//총 90개
 		
-		//byte------------
+		//byte
+		System.out.println("-----------byte-----------");
 	
-		//내 몸무게
+	
+		//1.내 몸무게
 		byte myWeight;
 		myWeight = 40;
 		System.out.println("내 몸무게: " + myWeight + "kg 입니다.");
 	
-		//이번 달
+		//2.우리 가족
+		byte myFamily;
+		myFamily = 4;
+		System.out.println("우리 가족: " + myFamily + "명 입니다.");
+		
+		//3.내 생월
+		byte birthMonth;
+		birthMonth = 1;
+		System.out.println("내 생월: " + birthMonth + "월 입니다.");
+		
+		//4.이번 달
 		byte thisMonth;
 		thisMonth = 9;
 		System.out.println("이번 달: " + thisMonth + "월 입니다.");
 		
-		//가족인원
-		byte 
+		//5.수소 분자량
+		byte h2Mole;
+		h2Mole = 2;
+		System.out.println("수소 분자량: " + h2Mole + "g/mol 입니다.");
+		
+		//6. 수업 시간
+		byte totalClassHour;
+		totalClassHour = 8;
+		System.out.println("총 수업 시간: " + totalClassHour + "시간 입니다.");
+		
+		//7. 점심 시간
+		byte lunchTime;
+		lunchTime = 1;
+		System.out.println("점심 시간: " + lunchTime + "시 입니다.");
+		
+		//8. 센터 문 닫는 시간
+		byte closeTimeOfCenter;
+		closeTimeOfCenter = 10;
+		System.out.println("센터 문 닫는 시간: " + closeTimeOfCenter + "pm 입니다.");
+		
+		//9. 내 나이
+		byte myAge;
+		myAge = 27;
+		System.out.println("내 나이: " + myAge + "살 입니다.");
+		
+		//10. 1다스
+		byte oneDozen;
+		oneDozen = 12;
+		System.out.println("1 다스: " + oneDozen + "자루 입니다.");
+		System.out.println();
+		
+		//short
+		System.out.println("----------short-----------");
+		
+		//1. 내 키
+		short myHeight;
+		myHeight = 155;
+		System.out.println("내 키: " + myHeight + "cm 입니다.");
+		
+		//2. ADA
+		short ada;
+		ada = 3080;
+		System.out.println("ADA: " + ada + "원 입니다.");
+		
+		//
+				
+		
 		
 	}
 
