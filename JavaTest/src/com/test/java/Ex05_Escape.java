b1c855c5ad076ecfcfc1049c262164fd497f80a6
umingming <u_0112@naver.com>
2021-09-10 15:11:37 +0900

    성적표 만들기

--- JavaTest/src/com/test/java/Ex05_Escape.java
+++ JavaTest/src/com/test/java/Ex05_Escape.java
@@ -4,21 +4,27 @@ public class Ex05_Escape {
 
 	public static void main(String[] args) {
 	
-		String str;
-		str = "안녕하세요.\r홍길동";
-		System.out.println(str);
-		
-		String str2 = "하나\t둘\t셋\t넷";
-		String str3 = "하나	둘	셋	넷";
-	    System.out.println(str2);
-	    System.out.println(str3);
-	    int a = 10;
-	    int b = 20;
-	    System.out.println( a + " + " + b + " = " + (a + b) ); 
-	    System.out.println( a + " + " + b + " = " + (a + b) ); 
-
-	    int jumin = 0x9ffcf;  
-	    System.out.println("주민번호: " + jumin);
+	    //성적표
+	    String name1 = "홍길동";
+	    int kor1 = 39;
+	    int eng1 = 35;
+	    int math1 = 35;
+	    
+	    String name2 = "아무개";
+	    int kor2 = 39, eng2 = 35, math2 = 35;
+	    
+	    System.out.println("=============================");
+	    System.out.println("            성적표");
+	    System.out.println("=============================");
+	    System.out.println("[이름]\t[국어]\t[영어]\t[수학]");
+	    System.out.println("=============================");
+	    System.out.print(name1+"\t");
+	    System.out.print(kor1+"\t");
+	    System.out.print(eng1+"\t");
+	    System.out.println(math1);
+	 
+	    System.out.println(name2 + "\t" + kor2 + "\t" + eng2 + "\t" + math2 + "\t" );
+	    
 	}
 
 }
