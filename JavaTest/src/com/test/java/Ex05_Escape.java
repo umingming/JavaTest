70163ebb01c8387ba0a43097b3a8b0f437916ee6
umingming <u_0112@naver.com>
2021-09-10 15:02:28 +0900

    변수

--- JavaTest/src/com/test/java/Ex05_Escape.java
+++ JavaTest/src/com/test/java/Ex05_Escape.java
@@ -12,7 +12,13 @@ public class Ex05_Escape {
 		String str3 = "하나	둘	셋	넷";
 	    System.out.println(str2);
 	    System.out.println(str3);
+	    int a = 10;
+	    int b = 20;
+	    System.out.println( a + " + " + b + " = " + (a + b) ); 
+	    System.out.println( a + " + " + b + " = " + (a + b) ); 
 
+	    int jumin = 0x9ffcf;  
+	    System.out.println("주민번호: " + jumin);
 	}
 
 }
--- JavaTest/src/com/test/java/homework/No1_DataType.java
+++ JavaTest/src/com/test/java/homework/No1_DataType.java
@@ -61,9 +61,9 @@ public class No1_DataType {
 		byte oneDozen;
 		oneDozen = 12;
 		System.out.println("1 다스: " + oneDozen + "자루 입니다.");
-		System.out.println();
 		
 		//short
+		System.out.println();
 		System.out.println("----------short-----------");
 		
 		//1. 내 키
@@ -76,10 +76,47 @@ public class No1_DataType {
 		ada = 3080;
 		System.out.println("ADA: " + ada + "원 입니다.");
 		
-		//
+		//3. 전세계 국가
+		short worldCountry;
+		worldCountry = 249;
+		System.out.println("전세계 국가: " + worldCountry + "국 입니다.");
 				
+		//4. 연어 가격
+		short salmonPrice;
+		salmonPrice = 14000;
+		System.out.println("연어 가격: " + salmonPrice + "원 입니다."); 
 		
+		//5. 월간 출생아수
+		short monthlyBirthRate;
+		monthlyBirthRate = 21526;
+		System.out.println("월간 출생아수: " + monthlyBirthRate + "명 입니다.");
 		
+		//int
+		System.out.println();
+		System.out.println("-----------int------------");
+		
+		//1. 대한민국 인구수
+		int korNum;
+		korNum = 51821669;
+		System.out.println("대한민국 인구수: " + korNum + "명 입니다.");
+		
+		//2. 연간 출생아수
+		int annualBirthRate;
+		annualBirthRate = 272337;
+		System.out.println("연간 출생아수: " + annualBirthRate + "명 입니다.");
+	
+		//3. 비트코인
+		int bitcoint;
+		bitcoint = 56321000;
+		System.out.println("비트코인: " + bitcoint + "krw 입니다.");
+		
+		//4. 재난지원금
+		int emergencyGrant;
+		emergencyGrant = 250000;
+		System.out.println("재난지원금: " + emergencyGrant + "원 입니다.");
+	
+		
+	
 	}
 
 }
