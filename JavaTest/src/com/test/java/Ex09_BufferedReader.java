bde21037c3c8ab6450c551cc07ea0df9f050a2e5
umingming <u_0112@naver.com>
2021-09-13 09:47:50 +0900

    입력 받은 대로 출력하기

--- JavaTest/src/com/test/java/Ex09_BufferedReader.java
+++ JavaTest/src/com/test/java/Ex09_BufferedReader.java
@@ -1,9 +1,22 @@
 package com.test.java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ex09_BufferedReader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader 
+		 = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("문자 입력: ");
+		int code = reader.read();  //문자 코드값 변환
+		System.out.printf("%c%n",code);
+		
+		
+		
+//		클래스 > 코드의 집합 > 구현된 기능의 집합
 
 	}
 
