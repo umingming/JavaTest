078ba6985bb1602b488f5c03206c77ee7dfddc7d
umingming <u_0112@naver.com>
2021-09-14 10:47:59 +0900

    논리연산자

--- JavaTest/src/com/test/java/Ex13_Operator.java
+++ JavaTest/src/com/test/java/Ex13_Operator.java
@@ -3,8 +3,26 @@ package com.test.java;
 public class Ex13_Operator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		System.out.println(b1 && b2);	//false
+		System.out.println(b1 || b2);	//true
+		System.out.println(!b1);		//false
+		System.out.println(!b2);		//true
 
+		//나이를 입력 > 19세 이상~ 60세 미만
+		int age = 25;
+		
+		System.out.println(age >= 19);
+		System.out.println(age < 60);
+//		System.out.println(19 <= age < 60);
+		System.out.println((age >= 19)&&(age < 60));
+		
+		int num = 9;
+		System.out.println((num % 2 ==0) && (num % 3 == 0));
+		System.out.println((num % 2 ==0) || (num % 3 == 0));
 	}
 
 }
