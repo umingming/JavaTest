9e0cb0b76f01c58fdbe2d482d07f3ad0f4f2aaeb
umingming <u_0112@naver.com>
2021-09-14 20:07:23 +0900

    예제 3-15

---
+++ JavaTest/src/com/java/ch3/OperatorEx15.java
@@ -0,0 +1,12 @@
+package com.java.ch3;
+
+public class OperatorEx15 {
+
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase - 32);
+		System.out.println(upperCase);
+
+	}
+
+}
