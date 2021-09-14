8976aec0b3bc98e13d48ac2bcac17afe6b04b721
umingming <u_0112@naver.com>
2021-09-14 15:08:38 +0900

    Q07

---
+++ JavaTest/src/com/test/question/operator/Q07.java
@@ -0,0 +1,24 @@
+package com.test.question.operator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q07 {
+
+	public static void main(String[] args) throws Exception{
+//		섭씨 온도를 입력 받아서 화씨 온도로 변환하시오.
+		
+//		설계>
+//		1. BufferedReader
+//		2. 섭씨(℃) : 출력
+//		3. 온도 입력
+//		4. 온도를 double로 변환
+//		5. 섭씨 ℃는 화씨 ℉입니다. 출력
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("씨(℃) : );
+		
+
+	}
+
+}
