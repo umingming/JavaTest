5de73e86163edc08f7f37c3fb07cc8a9669ef29d
umingming <u_0112@naver.com>
2021-09-15 00:40:47 +0900

    예제 3-18

---
+++ JavaTest/src/com/java/ch3/OperatorEx18.java
@@ -0,0 +1,14 @@
+package com.java.ch3;
+
+public class OperatorEx18 {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = (int)Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);
+		double PI = Math.PI;
+		System.out.println(PI);
+
+	}
+
+}
