db4e60266f88ada9e694bb438b08368159dcfd81
umingming <u_0112@naver.com>
2021-09-14 11:14:07 +0900

    연산 순서

--- JavaTest/src/com/test/java/Ex13_Operator.java
+++ JavaTest/src/com/test/java/Ex13_Operator.java
@@ -3,26 +3,35 @@ package com.test.java;
 public class Ex13_Operator {
 
 	public static void main(String[] args) {
+//		
+//		boolean b1 = true;
+//		boolean b2 = false;
+//		
+//		System.out.println(b1 && b2);	//false
+//		System.out.println(b1 || b2);	//true
+//		System.out.println(!b1);		//false
+//		System.out.println(!b2);		//true
+//
+//		//나이를 입력 > 19세 이상~ 60세 미만
+//		int age = 25;
+//		
+//		System.out.println(age >= 19);
+//		System.out.println(age < 60);
+////		System.out.println(19 <= age < 60);
+//		System.out.println((age >= 19)&&(age < 60));
+//		
+//		int num = 9;
+//		System.out.println((num % 2 ==0) && (num % 3 == 0));
+//		System.out.println((num % 2 ==0) || (num % 3 == 0));
 		
-		boolean b1 = true;
-		boolean b2 = false;
+//		int sum = 10 + 20 + 30;
+//		int sum = 30 + 30;
+//		int sum = 30 + 30;
+//		int sum = 60;
+//		sum;
 		
-		System.out.println(b1 && b2);	//false
-		System.out.println(b1 || b2);	//true
-		System.out.println(!b1);		//false
-		System.out.println(!b2);		//true
+		
 
-		//나이를 입력 > 19세 이상~ 60세 미만
-		int age = 25;
-		
-		System.out.println(age >= 19);
-		System.out.println(age < 60);
-//		System.out.println(19 <= age < 60);
-		System.out.println((age >= 19)&&(age < 60));
-		
-		int num = 9;
-		System.out.println((num % 2 ==0) && (num % 3 == 0));
-		System.out.println((num % 2 ==0) || (num % 3 == 0));
 	}
 
 }
