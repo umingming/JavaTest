a08f9a7fc8acf9a5437290ad5b4fc56c7474be2e
umingming <u_0112@naver.com>
2021-09-14 20:02:48 +0900

    예제 3-13

---
+++ JavaTest/src/com/java/ch3/OperatorEx13.java
@@ -0,0 +1,19 @@
+package com.java.ch3;
+
+public class OperatorEx13 {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		char c2 = (char)(c1 + 1);
+		char c3 = 'a' + 1;
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(++c1);
+		
+		int a = 1;
+		System.out.println(a++);
+		
+
+	}
+
+}
