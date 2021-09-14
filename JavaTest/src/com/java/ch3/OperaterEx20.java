63c1aeeb83dbaf6109cf4671ebb27bf374ab88cc
umingming <u_0112@naver.com>
2021-09-15 00:48:18 +0900

    예제 3-20

---
+++ JavaTest/src/com/java/ch3/OperaterEx20.java
@@ -0,0 +1,12 @@
+package com.java.ch3;
+
+public class OperaterEx20 {
+
+	public static void main(String[] args) {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+
+	}
+
+}
