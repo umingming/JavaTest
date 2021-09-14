ab395f3b23e951b48b729bf2c7feeac82a1dcebb
umingming <u_0112@naver.com>
2021-09-14 20:17:28 +0900

    예제 3-17

---
+++ JavaTest/src/com/java/ch3/OperatorEx16.java
@@ -0,0 +1,14 @@
+package com.java.ch3;
+
+public class OperatorEx16 {
+
+	public static void main(String[] args) {
+		float pi = 3.141592f;
+		float shortPi = (int)(pi * 1000) / 1000f; //3.142
+		System.out.println(shortPi);
+		float f = 10.0f/6;
+		System.out.println(f);
+
+	}
+
+}
