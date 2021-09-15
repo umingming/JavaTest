ba9cbde817934dcd5a442128e912c9e6e9a242cb
umingming <u_0112@naver.com>
2021-09-16 01:15:29 +0900

    복습

---
+++ JavaTest/src/com/java/ch6/Overloading.java
@@ -0,0 +1,16 @@
+package com.java.ch6;
+
+public class Overloading {
+
+	public static void main(String[] args) {
+	
+		add(3, 2);
+
+	}
+	public static int add(int a, int b) {
+		int add = a + b;
+		return add;
+	}
+	
+
+}
--- JavaTest/src/com/test/question/method/Q09.java
+++ JavaTest/src/com/test/question/method/Q09.java
@@ -7,14 +7,20 @@ public class Q09 {
 	public static void main(String[] args) {
 		System.out.print("현재 시간 : ");
 		nowTime();
+		nowTime_AMPM();
 		
 
 	}
 
 	private static void nowTime() {
 		Date now = new Date();
-		System.out.printf("%d시 %분 %초%n", now.getHours(), now.getMinutes(), now.getSeconds());
-		
+		System.out.printf("%d시 %d분 %d초%n", now.getHours(), now.getMinutes(), now.getSeconds());
 	}
 
+	private static void nowTime_AMPM() {
+		Date now = new Date();
+		String amPm = now.getHours() >= 12 ? "오후" : "오전";
+		int hours = now.getHours() >= 12 ? now.getHours() - 12 : now.getHours();
+		System.out.printf("%s %d시 %d분 %d초%n", amPm, hours, now.getMinutes(), now.getSeconds());
+	}
 }
