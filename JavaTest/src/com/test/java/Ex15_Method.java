7888cd89a4075ed104d9b7d20f23f9b37d43a2ae
umingming <u_0112@naver.com>
2021-09-15 09:23:28 +0900

    메소드

---
+++ JavaTest/src/com/test/java/Ex15_Method.java
@@ -0,0 +1,10 @@
+package com.test.java;
+
+public class Ex15_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- JavaTest/src/com/test/java/Ex_Method.java
+++ JavaTest/src/com/test/java/Ex_Method.java
@@ -11,7 +11,8 @@ public class Ex_Method {
 		System.out.println("안녕하세요.");
 	}
 
-
+//메소드 
+	//
 	
 
 }
