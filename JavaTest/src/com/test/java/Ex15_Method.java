6a4f262c45ff505bc3ef3bd81036b6abebc5bb81
umingming <u_0112@naver.com>
2021-09-15 10:11:33 +0900

    메소드

--- JavaTest/src/com/test/java/Ex15_Method.java
+++ JavaTest/src/com/test/java/Ex15_Method.java
@@ -3,8 +3,35 @@ package com.test.java;
 public class Ex15_Method {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//제어 흐름
+		System.out.println("하나");
+		m1();
+		System.out.println("둘");
+		m2();
+		System.out.println("셋");
+		m3();
+		m4();
+		int num = 1;
+		System.out.println(num);
+		
+		
+	}
 
+	private static void m4() {
+		System.out.println("금붕어");
+	}
+	
+	private static void m3() {
+		System.out.println("거북이");
+	}
+
+	public static void m1() {
+		System.out.println("강아지");
+	}
+
+	public static void m2() {
+		System.out.println("고양이");
 	}
 
 }
