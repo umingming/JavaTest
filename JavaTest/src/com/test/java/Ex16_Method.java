33c55f45b34c84f5a544ebe27daebfd99ba6e555
umingming <u_0112@naver.com>
2021-09-15 10:41:13 +0900

    인자

--- JavaTest/src/com/test/java/Ex16_Method.java
+++ JavaTest/src/com/test/java/Ex16_Method.java
@@ -4,10 +4,13 @@ public class Ex16_Method {
 
 	public static void main(String[] args) {
 		//요구사항] '홍길동'에게 인사를 하는 메소드를 만드시오.
-		hello();
-		hello2();
+		hi("이유미");
+		hi("이유미");
+		hi("이유미");
+		hi("이유미");
+		hi("이유미");
 		//수정사항] '아무개'에게 인사를 하는 메소드를 만드시오.
-	}
+	} //변수는 초기화하지 않은 상태(null)로 사용이 불가능.
 	public static void hello() {
 		String name = "홍길동";
 		System.out.println(name + "님 안녕하세요.");
