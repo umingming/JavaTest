99105f0281f81f8e2224d6e7d5b2039bf48ebb0f
umingming <u_0112@naver.com>
2021-09-16 23:24:00 +0900

    예제 4-1

---
+++ JavaTest/src/com/java/ch4/FlowEx1.java
@@ -0,0 +1,27 @@
+package com.java.ch4;
+
+public class FlowEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		System.out.printf("x=%d 일 때, 참인 것은%n", x);
+		
+		if(x==0) System.out.println("x==0"); //O
+		if(x!=0) System.out.println("x!=0"); //X
+		if(!(x==0)) System.out.println("!(x==0)"); //X
+		if(!(x!=0)) System.out.println("!(x!=0)"); //O
+		
+		x = 1;
+		System.out.printf("x=%d 일 때, 참인 것은%n", x);
+		
+		if(x==0) System.out.println("x==0"); //X
+		if(x!=0) System.out.println("x!=0"); //O
+		if(!(x==0)) System.out.println("!(x==0)"); //O
+		if(!(x!=0)) System.out.println("!(x!=0)"); //X
+		
+		
+
+	}
+
+}
