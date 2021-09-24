576c41701080073f44a08d84f5f9f0a7924ad8b9
umingming <u_0112@naver.com>
2021-09-24 19:06:07 +0900

    과제

--- JavaTest/src/com/test/question/iteration2/Q04.java
+++ JavaTest/src/com/test/question/iteration2/Q04.java
@@ -3,35 +3,33 @@ package com.test.question.iteration2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Q03 {
+public class Q04 {
 
 	public static void main(String[] args) throws Exception{
 		/*
+		*
+		 *
 		  *
-		 ***
-		*****
-		-행의 갯수 입력 받으시오.
+		-행의 갯수 입력 받기
 		
 		설계>
 		1. BufferedReader
 		2. 행의 갯수 입력 받기
-		3. for문 행 반복
-			>for문 행-i 반복 공백 출력
-			>forans 2*i+1 반복 * 출력
+		3. for문 입력한 수만큼 반복 
+			>for문 i보다 하나 적게 공백 반복 출력
+			>별 출력
 		 */
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("행의 갯수 : ");
+		System.out.print("행의 개수 : ");
 		int line = Integer.parseInt(reader.readLine());
+		
 		for(int i=0; i<line; i++) {
-			for(int j=0; j<line-i-1; j++) {
-				System.out.printf(" ");
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
 			}
-			
-			for(int j=0; j<2*i+1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			System.out.println("*");
 		}
 	}
+
 }
