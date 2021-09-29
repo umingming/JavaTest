4111606104a0bad06852fa12b5caaf645a08bcec
umingming <u_0112@naver.com>
2021-09-29 09:40:18 +0900

    과제

---
+++ JavaTest/src/com/test/java/array/Q04.java
@@ -0,0 +1,59 @@
+package com.test.java.array;
+
+import java.util.Arrays;
+
+public class Q04 {
+
+	public static void main(String[] args) {
+		/*
+		1~20 사이의 난수를 답고 있는 배열을 생성하고 최대값과 최소값을 출력하시오.
+		-난수를 20개 발생 후 배열에 넣는다
+		
+		설계>
+		1. 길이가 20인 int 배열 선언
+		2. 최대, 최소, 원본 변수 선언
+		3. for문 배열의 길이
+			>i에 난수 저장
+			>if문 최소, 최대 비교 
+				>해당 수가 배열 중 가장 크거나 가장 작으면 최대, 최소 변수에 저장
+		4.결과 출력 원본의 경우 substring사용
+		 */
+		
+		int[] nums = new int[20];
+		
+		int min = 20, max = 1;
+		String array = "";
+		
+		for(int i=0; i<nums.length; i++) {
+			nums[i] = (int)(Math.random() * 20) + 1;
+			if (min > nums[i]) {
+				min = nums[i];
+			}
+			if (max < nums[i]) {
+				max = nums[i];
+			}
+			
+			array += nums[i] + ", ";
+		}
+		
+		
+		System.out.println(Arrays.toString(nums));
+		System.out.printf("원본 : %s%n", array.substring(0, array.lastIndexOf(",")));
+		System.out.printf("최대값 : %d%n", max);
+		System.out.printf("최소값 : %d%n", min);
+
+	}
+
+	private static String String(int[] nums) {
+		String temp = "";
+		
+		for(int i=0; i<nums.length; i++) {
+			temp += nums[i] + ",";
+		}
+		
+		temp = temp.substring(0, temp.length()-1);
+		
+		return "["+temp+"]";
+	}
+
+}
---
+++ JavaTest/src/com/test/java/array/Q06.java
@@ -0,0 +1,65 @@
+package com.test.java.array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q06 {
+
+	public static void main(String[] args) throws Exception{
+		/*
+		중복되지 않는 임의의 숫자를 만드시오.
+		-숫자의 범위와 갯수를 입력 받으시오.
+		
+		설계>
+		1. BufferedReader
+		2. 최대, 최소, 개수 입력
+		3. 개수를 길이로 하는 int 배열 선언
+		4. for문 배열 길이 반복
+			>난수 저장
+			>for문 무한루프
+				>중복 변수 선언
+				>for문 i반복
+				>if문 num[i]가 num[j]와 같은지 확인
+					>같으면 중복 ++
+				>중복이 0이면 break
+				>난수 재저장
+		5. 출력
+		 */
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("최대 값 : ");
+		int max = Integer.parseInt(reader.readLine());
+
+		System.out.print("최소 값 : ");
+		int min = Integer.parseInt(reader.readLine());
+		
+		System.out.print("개수 : ");
+		int n = Integer.parseInt(reader.readLine());
+
+		int[] nums = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			nums[i] = (int)(Math.random() * (max - min + 1)) + min;
+			
+			for(;;) {
+				int overlap = 0;
+				for(int j=0; j<i; j++) {
+					if(nums[i] == nums[j]) {
+						overlap++;
+					}
+				}
+				
+				if(overlap == 0) {
+					break;
+				}
+				
+				nums[i] = (int)(Math.random() * (max - min + 1)) + min;
+			}
+		}
+		
+		System.out.println(Arrays.toString(nums));
+	}
+
+}
