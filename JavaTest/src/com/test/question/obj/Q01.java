ecc1255150c6c8a8f545a39f1c2e4d944e9188b9
umingming <u_0112@naver.com>
2021-09-30 18:32:47 +0900

    수정

--- JavaTest/src/com/test/question/obj/Q01.java
+++ JavaTest/src/com/test/question/obj/Q01.java
@@ -5,11 +5,24 @@ public class Q01 {
 	public static void main(String[] args) {
 		Book book = new Book();
 		book.setTitle("Java의 정석");
+		System.out.println(book.getTitle());
+		
 		book.setPrice(30000);
+		System.out.println(book.getPrice());
+		
 		book.setAuthor("남궁성");
+		System.out.println(book.getAuthor());
+		
 		book.setPage(980);
+		System.out.println(book.getPage());
+		
 		book.setPublisher("도우출판");
+		
+		System.out.println(book.getPubYear());
+		
 		book.setIsbn("978-89-94492-03-2");
+		System.out.println(book.getIsbn());
+		
 		System.out.println(book.info());
 	}
 
