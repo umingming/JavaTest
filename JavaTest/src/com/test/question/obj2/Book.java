c07379f1c32fa23d00985b72d18148878b6de4a2
umingming <u_0112@naver.com>
2021-10-01 09:40:35 +0900

    클래스 선생님이 풀어줌

--- JavaTest/src/com/test/question/obj2/Book.java
+++ JavaTest/src/com/test/question/obj2/Book.java
@@ -2,9 +2,91 @@ package com.test.question.obj2;
 
 public class Book {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String title;
+	private int price;
+	private String author;
+	private String publisher;
+	private String pubYear = "2021";
+	private String isbn;
+	private int pages;
 
+	public String getAuthor() {
+		return author;
 	}
 
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public String getPubYear() {
+		return pubYear;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	
+	public void setTitle(String title) {
+		if(isValid(title) && title.length() <= 50) {
+			this.title = title;
+		} else {
+			System.out.println("제목을 올바르게 입력하세요.");
+		}
+	}
+	
+	private boolean isValid(String title) {
+		
+		for(int i=0; i<title.length(); i++){
+			char c = title.charAt(i);
+			if((c < '가' || c > '힣') 
+				&& (c < 'A' || c > 'Z') 
+				&& (c < 'a' || c > 'z') 
+				&& (c < '0' || c > '9')
+				&& (c != ' ')) { 
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String info() {
+		String temp = "";
+		temp += "제목 " + this.title;
+		temp += ", 가격 " + this.price;
+		temp += ", 저자 " + this.author;
+		temp += ", 출판사 " + this.publisher;
+		temp += ", 출판년도 " + this.pubYear;
+		temp += ", 페이지 " + this.isbn;
+		
+		return temp;
+	}
 }
--- JavaTest/src/com/test/question/obj2/Q01.java
+++ JavaTest/src/com/test/question/obj2/Q01.java
@@ -3,7 +3,10 @@ package com.test.question.obj2;
 public class Q01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Book b1 = new Book();
+		
+		b1.setTitle("자바의 정석");
+		System.out.println(b1.info());
 
 	}
 
