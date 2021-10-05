03ead88c51119fa3aed587eebf9f4ff9c09c9d13
umingming <u_0112@naver.com>
2021-10-05 19:26:00 +0900

    주석 수정

--- JavaTest/src/com/test/question/obj/constructor/Q08.java
+++ JavaTest/src/com/test/question/obj/constructor/Q08.java
@@ -3,32 +3,26 @@ package com.test.question.obj.constructor;
 public class Q08 {
 
 	public static void main(String[] args) {
-		//시간 1
-		Time t1 = new Time(); //기본 생성자 호출
+		
+		Time t1 = new Time(); 
 		System.out.println(t1.info());
 
-		//시간 2
-		Time t2 = new Time(2, 30, 45); //오버로딩 생성자 호출
+		Time t2 = new Time(2, 30, 45); 
 		System.out.println(t2.info());
 
-		//시간 3
-		Time t3 = new Time(1, 70, 30); //오버로딩 생성자 호출
+		Time t3 = new Time(1, 70, 30);
 		System.out.println(t3.info());
 
-		//시간 4
-		Time t4 = new Time(30, 10); //오버로딩 생성자 호출
+		Time t4 = new Time(30, 10); 
 		System.out.println(t4.info());
 
-		//시간 5
-		Time t5 = new Time(90, 10); //오버로딩 생성자 호출
+		Time t5 = new Time(90, 10); 
 		System.out.println(t5.info());
 
-		//시간 6
-		Time t6 = new Time(50); //오버로딩 생성자 호출
+		Time t6 = new Time(50);
 		System.out.println(t6.info());
 
-		//시간 7
-		Time t7 = new Time(10000); //오버로딩 생성자 호출
+		Time t7 = new Time(10000); 
 		System.out.println(t7.info());
 
 	}
