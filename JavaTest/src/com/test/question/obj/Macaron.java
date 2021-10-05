b2b193e77331b7c837def1a84ac80b886c297a27
umingming <u_0112@naver.com>
2021-10-05 18:55:47 +0900

    클래스 분리

---
+++ JavaTest/src/com/test/question/obj/Macaron.java
@@ -0,0 +1,27 @@
+package com.test.question.obj;
+
+public class Macaron {
+	private int size;
+	private String color;
+	private int thickness;
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getThickness() {
+		return thickness;
+	}
+	public void setThickness(int thickness) {
+		this.thickness = thickness;
+	}
+	
+}
--- JavaTest/src/com/test/question/obj/Refrigerator.java
+++ JavaTest/src/com/test/question/obj/Refrigerator.java
@@ -78,35 +78,3 @@ public class Refrigerator {
 	
 
 }
-
-class Item {
-	private String name;
-	private Calendar expiration;
-	
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public Calendar getExpiration() {
-		return  this.expiration;
-	}
-	
-	public void setExpiration(String date) {
-		date = date.replace("-", "");
-		Calendar expiration = Calendar.getInstance();
-		
-		expiration.set(Integer.parseInt(date.substring(0, 4)),
-				Integer.parseInt(date.substring(4, 6)) -1,
-				Integer.parseInt(date.substring(6)));
-		
-		this.expiration = expiration;
-	}//매개변수가 String일 경우 Calendar로 변환해 저장
-	
-	public void setExpiration(Calendar date) {
-		this.expiration = expiration;
-	}
-}
