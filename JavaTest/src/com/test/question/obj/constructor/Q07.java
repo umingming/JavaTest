2921fe6b86416e0079fef808bb194a37ed5e3a29
umingming <u_0112@naver.com>
2021-10-05 16:54:19 +0900

    미정? 0으로 두면 안 되나

---
+++ JavaTest/src/com/test/question/obj/constructor/Q07.java
@@ -0,0 +1,24 @@
+package com.test.question.obj.constructor;
+
+public class Q07 {
+
+	public static void main(String[] args) {
+		//학생 1
+		Student s1 = new Student(); //기본 생성자 호출
+		System.out.println(s1.info());
+
+		//학생 2
+		Student s2= new Student("홍길동", 13); //오버로딩 생성자 호출
+		System.out.println(s2.info());
+
+		//학생 3
+		Student s3= new Student(3, 10, 30); //오버로딩 생성자 호출
+		System.out.println(s3.info());
+
+		//학생 4
+		Student s4= new Student("아무개", 12, 1, 5, 11); //오버로딩 생성자 호출
+		System.out.println(s4.info());
+
+	}
+
+}
---
+++ JavaTest/src/com/test/question/obj/constructor/Student.java
@@ -0,0 +1,49 @@
+package com.test.question.obj.constructor;
+
+public class Student {
+	/*
+	설계>
+	1. 멤버 변수; 이름, 나이, 학년, 반, 번호
+	2. 생성자 메소드
+		>기본 생성자; 미정, 0, 0, 0, 0
+		>오버로딩 매개변수(이름, 나이, 학년, 반, 번호)
+		>오버로딩 매개변수(이름, 나이)
+		>오버로딩 매개변수(학년, 반, 번호)
+	3. info 메소드
+	 */
+	
+	private String name;
+	private int age;
+	private int grade;
+	private int classNumber;
+	private int number;
+	
+	public Student() {
+		this("미정", 0, 0, 0, 0);
+	}
+	
+	public Student(String name, int age, int grade, int classNumber, int number) {
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
+		this.classNumber = classNumber;
+		this.number = number;
+	}
+	
+	public Student(String name, int age) {
+		this(name, age, 0, 0, 0);
+	}
+	
+	public Student(int grade, int classNumber, int number) {
+		this("미정", 0, grade, classNumber, number);
+	}
+	
+	public String info() {
+		String temp = this.name + 
+				"(나이 : " + this.age + "세" +
+				", 학년 : " + this.grade + 
+				", 반 : " + this.classNumber + 
+				", 번호 : " + this.number + ")"; 
+		return temp;
+	}
+}
