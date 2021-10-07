640b24a349f655447ca24d4b4f7a4e0b8dd5e0c5
umingming <u_0112@naver.com>
2021-10-07 16:57:18 +0900

    체크 메소드 생성

--- JavaTest/src/com/test/question/scanner/Q01.java
+++ JavaTest/src/com/test/question/scanner/Q01.java
@@ -8,37 +8,40 @@ public class Q01 {
 		-숫자가 5개 입력되지 않았을 경우 안내 멘트 출력.
 		
 		설계>
-		1. 카운트, input 정적 변수 선언
+		1. 카운트, input, String 배열 정적 변수 선언
 		2. 메인 메소드
 			>Scanner으로 숫자 입력 받음
 			>확인, 결과 메소드
-		3. 확인 메소드
+		3. 카운트 메소드
 			>input.split 이용해 배열 생성 후 요소에 값 저장
 			>for문으로 배열 길이 반복
 				>요소를 int로 형변환해 변수에 저장
 				>if문 양수/음수? 카운트++
+		4. 확인 메소드
 			>if문 길이가 5인지? 아니면 안내 멘트 출력
-		4. 결과 메소드
+		5. 결과 메소드
 			>printf로 카운트 정적변수 출력
 	 */
 	
-	private static String input;
 	private static int positive;
 	private static int negative;
 	private static int zero;
+	private static String input;
+	private static String[] nums;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("입력: ");
 		input = scan.nextLine();
 		
-		check();
+		count();
 		result();
+		check();
 
 	}//main
 
-	private static void check() {
-		String[] nums = input.split(" ");
+	private static void count() {
+		nums = input.split(" ");
 		
 		for(int i=0; i<nums.length; i++) {
 			int n = Integer.parseInt(nums[i]);
@@ -51,13 +54,7 @@ public class Q01 {
 				zero++;
 			}
 		}
-		
-		if(nums.length < 5) {
-			System.out.printf("숫자가 %d개 부족합니다.%n", 5-nums.length);
-		}else if(nums.length > 5) {
-			System.out.printf("숫자가 %d개 많습니다.%n", nums.length-5);
-		}
-	}//확인
+	}//카운트
 
 	private static void result(){
 		System.out.println("[결과]");
@@ -65,4 +62,12 @@ public class Q01 {
 		System.out.printf("음수: %d개%n", negative);
 		System.out.printf("0: %d개%n", zero);
 	}//결과 출력
+	
+	private static void check() {
+		if(nums.length < 5) {
+			System.out.printf("숫자가 %d개 부족합니다.%n", 5-nums.length);
+		}else if(nums.length > 5) {
+			System.out.printf("숫자가 %d개 많습니다.%n", nums.length-5);
+		}
+	}//체크
 }
