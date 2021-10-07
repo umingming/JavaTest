9c7199289c70a5ff5c85cd970d2185368b3f30c4
umingming <u_0112@naver.com>
2021-10-07 15:45:50 +0900

    잘하네

--- JavaTest/src/com/test/question/scanner/Q01.java
+++ JavaTest/src/com/test/question/scanner/Q01.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Q01 {
 	/*
 		숫자를 입력 받아 양수, 음수의 개수를 출력하시오.
+		-숫자가 5개 입력되지 않았을 경우 안내 멘트 출력.
 		
 		설계>
 		1. 카운트, input 정적 변수 선언
 		2. 메인 메소드; Scanner으로 숫자 입력 받음
 		3. 확인 메소드
-			>input.split 이용해 배열 생성
+			>input.split 이용해 배열 생성 후 요소에 값 저장
 			>for문으로 배열 길이 반복
-				>배열의 요소에 값 저장
+				>요소를 int로 형변환해 변수에 저장
+				>if문 양수/음수? 카운트++
 			>if문 길이가 5인지? 아니면 안내 멘트 출력
-		4. 카운트 메소드
-			>if문으로 숫자 확인하고 정적변수++
-		5. 결과 메소드
+		4. 결과 메소드
 			>printf로 카운트 정적변수 출력
 	 */
 	
@@ -24,7 +24,6 @@ public class Q01 {
 	private static int positive;
 	private static int negative;
 	private static int zero;
-	
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -38,8 +37,17 @@ public class Q01 {
 
 	private static void check() {
 		String[] nums = input.split(" ");
+		
 		for(int i=0; i<nums.length; i++) {
-			count(Integer.parseInt(nums[i]));
+			int n = Integer.parseInt(nums[i]);
+			
+			if(n > 0) {
+				positive++;
+			} else if(n < 0) {
+				negative++;
+			} else {
+				zero++;
+			}
 		}
 		
 		if(nums.length < 5) {
@@ -49,21 +57,10 @@ public class Q01 {
 		}
 	}//확인
 
-	private static void count(int i) {
-		if(i > 0) {
-			positive++;
-		} else if(i < 0) {
-			negative++;
-		} else {
-			zero++;
-		}
-	}//카운트
-
 	private static void result(){
 		System.out.println("[결과]");
 		System.out.printf("양수: %d개%n", positive);
 		System.out.printf("음수: %d개%n", negative);
 		System.out.printf("0: %d개%n", zero);
 	}//결과 출력
-
 }
--- JavaTest/src/com/test/question/scanner/Q02.java
+++ JavaTest/src/com/test/question/scanner/Q02.java
@@ -1,15 +1,80 @@
 package com.test.question.scanner;
 
+import java.util.Scanner;
+
 public class Q02 {
 	/*
 	토큰을 입력 받아 각각의 개수를 출력하시오.
 	
-	ㅅ
+	설계>
+	1. 카운트, input 정적 변수 선언
+	2. 메인 메소드
+		>Scanner
+		>nextLine으로 입력값 input에 저장
+	3. 확인 메소드
+		>input.split으로 String 배열 생성해 요소에 값 저장
+		>for문 배열 길이 반복
+			>카운트 메소드
+	4. 카운트 메소드(매개변수 String)
+		>boolean 변수 선언후 true로 초기화
+		>for문 string의 길이 반복
+			>c 변수 선언 후 String의 i번째 글자로 초기화
+			>if문 c가 숫자/문자가 아닌지? 논리 변수 false로 저장
+				>문자확인할 때 온점도 포함
+		>if 숫자/문자? 해당 카운트++
+	5. 결과 메소드; printf로 정적 변수 출력
 	 */
+	
+	private static String input;
+	private static int numCount;
+	private static int strCount;
+	private static int mixCount;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("입력: ");
+		input = scan.nextLine();
+		
+		check();
+		result();
 
-	}
+	}//main
 
+	private static void check() {
+		String[] str = input.split(" ");
+		
+		for(int i=0; i<str.length; i++) {
+			count(str[i]);
+		}
+	}//확인
+
+	private static void count(String str) {
+		boolean isNum = true;
+		boolean isStr = true;
+		
+		for(int i=0; i<str.length(); i++) {
+			char c = str.charAt(i);
+			
+			if(c < '0' || c > '9') {
+				isNum = false;
+			} else if(c < '가' || c > '힣' || c != '.' ) {
+				isStr = false;
+			}
+		}
+		
+		if(isNum) {
+			numCount++;
+		} else if(isStr) {
+			strCount++;
+		} else {
+			mixCount++;
+		}
+	}//카운트
+	
+	private static void result() {
+		System.out.println("[결과]");
+		System.out.printf("숫자 : %d개%n", numCount);
+		System.out.printf("문자 : %d개%n", strCount);
+		System.out.printf("혼합 : %d개%n", mixCount);
+	}//결과
 }
