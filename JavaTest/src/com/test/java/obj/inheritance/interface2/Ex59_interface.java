f47393da24f75b5c35f1a76413bedb581b6b8757
umingming <u_0112@naver.com>
2021-10-13 11:41:18 +0900

    클래스가 인터페이스를 상속 받을 때 인터페이스의 메소드를 구현해야 함.

--- JavaTest/src/com/test/java/obj/inheritance/interface2/Ex59_interface.java
+++ JavaTest/src/com/test/java/obj/inheritance/interface2/Ex59_interface.java
@@ -3,8 +3,57 @@ package com.test.java.obj.inheritance.interface2;
 public class Ex59_interface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Gram lg = new Gram();
+//		
+//		lg.start();
+//		lg.stop();
+//		
+//		MacBook mac = new MacBook();
+//		
+//		mac.start();
+		
+		Laptop lg = new Gram();
+		lg.start();
+		
+		Laptop mac = new MacBook();
+		
 
 	}
 
+}
+
+interface Laptop {
+	void start();
+	void stop();
+}
+class Gram implements Laptop{
+	public String model;
+	public String color;
+	
+	@Override
+	public void start() {
+		System.out.println("전원을 켭니다.");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("전원을 끕니다.");
+	}
+}
+
+class MacBook implements Laptop{
+	
+	public String model;
+	public int weight;
+	
+	@Override
+	public void start() {
+		System.out.println("맥북 On");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("맥북 Off");
+	}
+	
 }
