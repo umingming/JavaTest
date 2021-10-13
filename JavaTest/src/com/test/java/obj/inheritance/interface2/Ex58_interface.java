18d69e230308b972e5b7d2f4433fcc676b07dfe9
umingming <u_0112@naver.com>
2021-10-13 10:35:00 +0900

    인터페이스

--- JavaTest/src/com/test/java/obj/inheritance/interface2/Ex58_interface.java
+++ JavaTest/src/com/test/java/obj/inheritance/interface2/Ex58_interface.java
@@ -3,8 +3,34 @@ package com.test.java.obj.inheritance.interface2;
 public class Ex58_interface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		
+		
 
 	}
 
+}
+
+interface Member {
+	
+//	public int a;
+//	public int b;
+	
+	public abstract void test();
+	int a(int b);
+	
+}
+
+class Hong implements Member{
+	
+	public void test() {
+		
+	}
+
+	@Override
+	public int a(int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
