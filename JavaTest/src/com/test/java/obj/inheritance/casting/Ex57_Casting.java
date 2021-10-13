ac06e132159a4f29bd33cdbf34db59d1418542bc
umingming <u_0112@naver.com>
2021-10-13 09:45:29 +0900

    다운캐스팅은 조삼모사로만 가능함.

--- JavaTest/src/com/test/java/obj/inheritance/casting/Ex57_Casting.java
+++ JavaTest/src/com/test/java/obj/inheritance/casting/Ex57_Casting.java
@@ -3,44 +3,31 @@ package com.test.java.obj.inheritance.casting;
 public class Ex57_Casting {
 
 	public static void main(String[] args) {
-		Parent p1 = new Parent();
-		System.out.println(p1.a);
-		System.out.println(p1.b);
-
-		Child c1 = new Child();
-		System.out.println(c1.a);
-		System.out.println(c1.b);
-		System.out.println(c1.c);
-		System.out.println(c1.d);
-		
-		Parent p2;
-		Child c2 = new Child();
-		
-		p2 = c2;
-		c2.a = 50;
-		
-		System.out.println(p2.a);
-		System.out.println(p2.b);
-		
+//		P
 //		Parent p3 = new Parent();
 //		Child c3;
 //		
 //		c3 = (Child)p3;
 //		System.out.println(c3.a);
 //		System.out.println(c3.b);
+//		
+//		Parent p4;
+//		Child c4 = new Child();
+//		
+//		p4 = c4;
+//		
+//		Child c5;
+//		c5 = (Child)p4;
+//		
+//		System.out.println(c5.a);
+//		System.out.println(c5.b);
+//		System.out.println(c5.c);
+//		System.out.println(c5.d);
 		
-		Parent p4;
-		Child c4 = new Child();
+		Daughter d = new Daughter();
+		Parent p = d;
 		
-		p4 = c4;
 		
-		Child c5;
-		c5 = (Child)p4;
-		
-		System.out.println(c5.a);
-		System.out.println(c5.b);
-		System.out.println(c5.c);
-		System.out.println(c5.d);
 	}
 
 }
@@ -54,5 +41,14 @@ class Parent {
 class Child extends Parent {
 	public int c = 30;
 	public int d = 40;
+	
+}
+
+class Son extends Parent {
+	public int e = 50;
+}
+
+class Daughter extends Parent {
+	public int f = 60;
 	
 }
---
+++ JavaTest/src/com/test/java/obj/inheritance/interface2/Ex58_interface.java
@@ -0,0 +1,10 @@
+package com.test.java.obj.inheritance.interface2;
+
+public class Ex58_interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
