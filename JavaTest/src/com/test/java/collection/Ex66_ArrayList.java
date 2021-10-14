a965986b0e17842b94011900dbdc5e7300861b03
umingming <u_0112@naver.com>
2021-10-14 12:15:50 +0900

    about collection

---
+++ JavaTest/src/com/test/java/collection/Ex66_ArrayList.java
@@ -0,0 +1,30 @@
+package com.test.java.collection;
+
+public class Ex66_ArrayList {
+
+	public static void main(String[] args) {
+		/*
+		컬렉션, collection
+		JCF, Java Collection Framework
+		향상된 배열
+		기존의 순수 배열을 성능이나 사용법에 버전 업한 클래스형태의 배열
+		길이 가변
+		1. 사용법
+		2. 구조
+		자료구조
+		 */
+		
+		/*
+		ArrayList 클래스
+		 */
+		
+		m1();
+
+	}
+
+	private static void m1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
