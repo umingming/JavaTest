d8356a9b1e5a68654a0e5bf8d2a7beb8b4ce42f7
umingming <u_0112@naver.com>
2021-10-20 10:38:55 +0900

    amazing

--- JavaTest/src/com/test/java/file/Ex72_File.java
+++ JavaTest/src/com/test/java/file/Ex72_File.java
@@ -1,10 +1,33 @@
 package com.test.java.file;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Ex72_File {
 
 	public static void main(String[] args) {
+		m1();
 		
 
+	}
+
+	private static void m1() {
+		try {
+			FileOutputStream stream 
+			= new FileOutputStream("C:\\class\\java\\file\\list.txt");
+			
+			stream.write(65);
+			stream.write(66);
+			stream.write(67);
+			
+			stream.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} //프로그램 -> 파일
+		
+		
+		
 	}
 
 }
