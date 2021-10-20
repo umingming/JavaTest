1e955c1d793b613d95832c170f5de921b16d58d4
umingming <u_0112@naver.com>
2021-10-20 18:08:45 +0900

    Q06

--- JavaTest/src/com/test/question/fileio/Path.java
+++ JavaTest/src/com/test/question/fileio/Path.java
@@ -9,7 +9,7 @@ public class Path {
 	static String Q04 = "C:\\class\\java\\file\\파일_입출력_문제\\단일검색.dat";
 	static String Q05User = "C:\\class\\java\\file\\파일_입출력_문제\\검색_회원.dat";
 	static String Q05Order = "C:\\class\\java\\file\\파일_입출력_문제\\검색_주문.dat";
-	static String Q06 = "C:\\class\\java\\file\\파일_입출력_문제\\괄호.dat";
+	static String Q06 = "C:\\class\\java\\file\\파일_입출력_문제\\괄호.java";
 	static String Q07 = "C:\\class\\java\\file\\파일_입출력_문제\\출결.dat";
 
 }
---
+++ JavaTest/src/com/test/question/fileio/Q06.java
@@ -0,0 +1,48 @@
+package com.test.question.fileio;
+
+import java.io.FileReader;
+import java.util.Stack;
+
+public class Q06 {
+
+	public static void main(String[] args) {
+		/*
+		모든 괄호가 서로 짝이 맞는지 검사하시오.
+		
+		1. FileReader, Stack 객체 생성
+		2. int 변수 선언 후 초기화.
+		3. while문 char가 -1이 아닐 때까지 반복함.
+			>if char이 (, {인지? Stack에 추가함
+			>if char이 ), }인지? Stack에서 꺼냄.
+		4. stream 종료
+		5. if Stack 크기가 0인지?
+			>올바른/ 올바르지 않은 소스입니다.
+		 */
+		
+		try {
+			FileReader reader = new FileReader(Path.Q06);
+			Stack<Integer> stack = new Stack<Integer>();
+			
+			int ch = 0;
+			
+			while((ch = reader.read()) != -1) {
+				if(ch == '(' || ch == '{') {
+					stack.push(ch);
+				} else if (ch == ')' || ch == '}') {
+					stack.pop();
+				}
+			}
+			
+			reader.close();
+			
+			if(stack.size() == 0) {
+				System.out.println("올바른 소스입니다.");
+			} else {
+				System.out.println("올바르지 않은 소스입니다.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
