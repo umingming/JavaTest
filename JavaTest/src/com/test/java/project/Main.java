cf57836db1095273a8d696d16f8c7d5c4329c53f
umingming <u_0112@naver.com>
2021-10-22 10:03:34 +0900

    using memeber

---
+++ JavaTest/src/com/test/java/project/Main.java
@@ -0,0 +1,16 @@
+package com.test.java.project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		1. 프로젝트 > 데이터
+		2. UI
+		3. 파일 읽기/쓰기 + 컬렉션 > 조합
+		
+		
+		 */
+
+	}
+
+}
