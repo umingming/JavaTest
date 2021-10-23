1567f731e60c465ddf5d7ee44330d8baea601a85
umingming <u_0112@naver.com>
2021-10-23 20:35:42 +0900

    Title

---
+++ JavaTest/src/com/test/java/project/Test.java
@@ -0,0 +1,13 @@
+package com.test.java.project;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		System.out.println("🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉");
+		System.out.println("🐉이기구 예매\t\t🐉");
+		System.out.println("🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉");
+
+	}
+
+}
