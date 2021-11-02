cb6c0538c32823891d2f72e63b2cb73c95989e32
umingming <u_0112@naver.com>
2021-11-02 12:02:35 +0900

    reg

--- JavaTest/src/com/test/java/etc/Ex89_RegEx.java
+++ JavaTest/src/com/test/java/etc/Ex89_RegEx.java
@@ -3,7 +3,15 @@ package com.test.java.etc;
 public class Ex89_RegEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 정규 표현식
+		 * 정규식
+		 * 문자열을 검색한다
+		 * 문자열을 대상으로 찾고자 하는 패턴을 가지는 문자열을 검색한다.
+		 * 자바와 상관없는 독립적인 표현식
+		 * 
+		 * 홍+길동 -> 홍홍길길동
+		 */
 
 	}
 
