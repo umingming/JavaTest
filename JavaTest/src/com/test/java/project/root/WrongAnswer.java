1ea13261b014b240789576dba91debe295553687
umingming <u_0112@naver.com>
2021-12-12 19:03:44 +0900

    오답 수정

--- JavaTest/src/com/test/java/project/root/WrongAnswer.java
+++ JavaTest/src/com/test/java/project/root/WrongAnswer.java
@@ -8,22 +8,21 @@ public class WrongAnswer {
 
 	public static void main(String[] args) throws Exception{
 		
-		String path = "C:\\Users\\u_011\\git\\DeepRoot\\DeepRoot\\src\\com\\data\\오답_공책.txt";
+		String path = "C:\\Users\\u_011\\git\\DeepRoot\\DeepRoot\\src\\com\\data\\오답.txt";
 		BufferedWriter writer = new BufferedWriter(new FileWriter(path));
 		
 		Random rnd = new Random();
 		
-		for(int i=0; i<100; i++) {
+		for(int i=0; i<300; i++) {
 			String userNum = "U0001";
 			String cateNum = "C" + (i/20 + 1);
 			int qNum = i%20 + 1;
 			int y = 20211201;
-			String content = String.format("S%08d%s%sQ%03d■%d"
-							, i*i*i+i+1
-							, userNum
-							, cateNum
-							, qNum
-							, y + i/10);
+			String content = String.format("%d■%d■%d■%d"
+							, i+1
+							, i/30+1
+							, i%30+1
+							, y+(i%30)/3);
 			
 			System.out.println(content);
 			writer.write(content);
