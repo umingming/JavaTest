59138609f36961fbbb3f53d9ac8ff40896460e79
umingming <u_0112@naver.com>
2021-12-12 15:54:49 +0900

    rename class

--- JavaTest/src/com/test/java/project/root/Rank.java
+++ JavaTest/src/com/test/java/project/root/Rank.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Random;
 
-public class Study {
+public class Rank {
 
 	public static void main(String[] args) throws Exception{
 		
--- JavaTest/src/com/test/java/project/root/Study.java
+++ JavaTest/src/com/test/java/project/root/Study.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Random;
 
-public class Rank {
+public class Study {
 
 	public static void main(String[] args) throws Exception{
 		
