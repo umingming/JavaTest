6b4297f5e48d5090a614d413170ef56fa1cedd1f
umingming <u_0112@naver.com>
2021-12-12 19:06:18 +0900

    학습 데이터 수정

--- JavaTest/src/com/test/java/project/root/Study.java
+++ JavaTest/src/com/test/java/project/root/Study.java
@@ -8,23 +8,19 @@ public class Study {
 
 	public static void main(String[] args) throws Exception{
 		
-		String path = "C:\\Users\\u_011\\git\\DeepRoot\\DeepRoot\\src\\com\\data\\회원_학습.txt";
-		BufferedWriter writer = new BufferedWriter(new FileWriter(path, true));
+		String path = "C:\\Users\\u_011\\git\\DeepRoot\\DeepRoot\\src\\com\\data\\학습.txt";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(path));
 		
 		Random rnd = new Random();
 		
-		for(int i=0; i<800; i++) {
+		for(int i=0; i<1000; i++) {
 			String userNum = "U0004";
 			String cateNum = "C" + (i/100 + 1);
 			int qNum = i%100 + 1;
-			String content = String.format("S%08d%s%sQ%03d■%s■%sQ%03d■%s"
-							, i+1701
-							, userNum
-							, cateNum
-							, qNum
-							, userNum
-							, cateNum
-							, qNum
+			String content = String.format("%d■%d■%d■%s"
+							, i+1
+							, i/100 + 1
+							, i%100 + 1
 							, "true");
 //							, rnd.nextBoolean());
 			
