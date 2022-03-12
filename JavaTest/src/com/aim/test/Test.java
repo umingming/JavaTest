a0659531db61adb8a7d8f33e59a462102d80c35c
umingming <u_0112@naver.com>
2022-03-13 00:05:20 +0900

    Add Parent tag to select

--- JavaTest/src/com/aim/test/Test.java
+++ JavaTest/src/com/aim/test/Test.java
@@ -17,48 +17,16 @@ public class Test {
 			File file = new File(pathOPC_TagMap_Mill);
 			FileInputStream stream = new FileInputStream(file);
 			Document document = Jsoup.parse(stream, null, "", Parser.xmlParser());
-//			System.out.println(document.toString());
 			
-			document.select("Item[Name=M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex]").attr("Point", "10000");
-//			Elements element = document.select("Item[Name=M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex]");
-//			element.attr("Point", "10000");
 			
-//			
-//			System.out.println(element.toString());
+			document.select("Block[Name=CoilRollingDataSendData_M1] Item[Name=M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex]").attr("Point", "10000");
+			
 			System.out.println(document.toString());
 			
-			
-//			Elements contents = document.select("item[Name=M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex]");
-//			String text = contents.text();
-//			System.out.println(text);
-					
-//			Elements body = Jsoup.connect(pathOPC_TagMap_Mill).get().select("M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex");
-//			System.out.println(body.toString());
-			
-//			Document document = parse(pathOPC_TagMap_Mill);
-//			print(document);
-//			edit(document);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-//	private static Document parse(String path) throws Exception {
-//		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//		DocumentBuilder builder = factory.newDocumentBuilder();
-//		return builder.parse(path);
-//	}
-//	
-//	private static void print(Document document) throws Exception {
-//		Transformer former = TransformerFactory.newInstance().newTransformer();
-//		Writer writer = new StringWriter();
-//		former.transform(new DOMSource(document), new StreamResult(writer));
-//		System.out.println(writer.toString());
-//	}
-//	
-//	private static void edit(Document document) {
-////		Element root = document.getDocumentElement();
-//		System.out.println(root);
-//	}
 }
