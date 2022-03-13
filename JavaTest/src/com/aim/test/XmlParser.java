03a99d591aa7415d787d278baa8d603331e56b29
umingming <u_0112@naver.com>
2022-03-13 21:28:42 +0900

    Add annotation

--- JavaTest/src/com/aim/test/XmlParser.java
+++ JavaTest/src/com/aim/test/XmlParser.java
@@ -7,6 +7,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.parser.Parser;
 
+/*
+	xml parser
+	- xml 파일 객체화, 내용 출력, 수정
+ */
 public class XmlParser {
 	private String xml;
 	private Document document;
@@ -15,17 +19,26 @@ public class XmlParser {
 		this.xml = xml;
 		parse();
 	}
-
+	
+	/*
+		document를 xml 파싱 값으로 초기화
+	 */
 	public void parse() throws Exception {
 		File file = new File(xml);
 		FileInputStream stream = new FileInputStream(file);
 		document = Jsoup.parse(stream, null, "", Parser.xmlParser());
 	}
 	
+	/*
+		파일 콘솔에 출력
+	 */
 	public void print() {
 		System.out.println(document.toString());
 	}
 	
+	/*
+		해당 쿼리의 속성을 원하는 값으로 수정
+	 */
 	public void modifyAttr(String query, String attr, String value) {
 		document.select(query)
 				.attr(attr, value);
