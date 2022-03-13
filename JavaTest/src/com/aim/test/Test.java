46b4abf3154061ebd7c7ce7cf8e16a1f66c68010
umingming <u_0112@naver.com>
2022-03-13 21:33:44 +0900

    Add variable to modify

--- JavaTest/src/com/aim/test/Test.java
+++ JavaTest/src/com/aim/test/Test.java
@@ -6,7 +6,19 @@ public class Test {
 		try {
 			String xml = "C:\\aim\\220311\\OPC_TagMap_Mill_0.75.xml";
 			XmlParser parser = new XmlParser(xml);
+			
 			parser.print();
+			
+			String query = String.format("Block[Name=%s] Item[Name=%s]"
+										, "CoilRollingDataSendData_M1"
+										, "M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex");
+			String attribute = "Point";
+			String value = "10000";
+			
+			parser.modifyAttr(query, attribute, value);
+			
+			
+			
 			
 			
 		} catch (Exception e) {
