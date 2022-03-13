1616d8965baefa3b2df85e5892281a9a59e8a53b
umingming <u_0112@naver.com>
2022-03-13 22:13:35 +0900

    Save new XML

--- JavaTest/src/com/aim/test/JdomTest.java
+++ JavaTest/src/com/aim/test/JdomTest.java
@@ -2,6 +2,7 @@ package com.aim.test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
@@ -11,12 +12,13 @@ import org.jdom2.output.XMLOutputter;
 public class JdomTest {
 	public static void main(String[] args) throws Exception {
 		String xml = "C:\\aim\\220311\\OPC_TagMap_Mill_0.75.xml";
-		String xmlNew = "C:\\aim\\220311\\OPC_TagMap_Mill_0.76.xml";
+		String xmlNew = "C:\\aim\\220311\\OPC_TagMap_Mill_0.77.xml";
 		
 		SAXBuilder builder = new SAXBuilder();
 		Document doc = builder.build(xml);
 		
 		XMLOutputter xout = new XMLOutputter();
+		xout.output(doc, new FileOutputStream(xmlNew));
 		xout.output(doc, System.out);
 	}
 }
