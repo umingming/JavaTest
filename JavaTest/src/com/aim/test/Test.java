185872a42b55a7ef5dc5ccad326eab2e35371e76
YouMe <87955005+umingming@users.noreply.github.com>
2022-03-14 09:29:21 +0900

    Update relative path

--- JavaTest/src/com/aim/test/Test.java
+++ JavaTest/src/com/aim/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		try {
-			String xml = "C:\\aim\\220311\\OPC_TagMap_Mill_0.75.xml";
+			String xml = "C:\\Aim\\Spring\\JavaTest\\OPC_TagMap_Mill_0.75.xml";
 			XmlParser parser = new XmlParser(xml);
 			
 //			parser.print();
