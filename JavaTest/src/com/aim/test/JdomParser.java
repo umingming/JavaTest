2c8c7207e3678e2719fa22ff181e3961b9f23f6b
YouMe <87955005+umingming@users.noreply.github.com>
2022-03-14 14:26:59 +0900

    Update JDOM file

--- JavaTest/src/com/aim/test/JdomParser.java
+++ JavaTest/src/com/aim/test/JdomParser.java
@@ -1,5 +1,87 @@
-package com.aim.test;
+package com.parser.jdom;
 
+import java.io.FileOutputStream;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+/*
+	XML Parser
+	- XML 파일 객체화, 내용 출력, 수정
+ */
 public class JdomParser {
+	private String xmlPath;
+	private Document doc;
+	private XMLOutputter xout; 
+	
+	public JdomParser(String xmlPath) throws Exception {
+		this.xmlPath = xmlPath;
+		parse();
+	}
+	
+	/*
+		doc를 xml 파싱 값으로 초기화
+	 */
+	public void parse() throws Exception {
+		SAXBuilder builder = new SAXBuilder();
+		this.doc = builder.build(xmlPath);
+	}
+	
+	/*
+		콘솔에 내용 출력
+	 */
+	public void print() throws Exception {
+		xout = new XMLOutputter();
+		Format format = xout.getFormat();
+		
+		format.setLineSeparator("\r\n");
+		format.setIndent("\t");
+		format.setTextMode(Format.TextMode.TRIM);
+		xout.setFormat(format);
+		
+		xout.output(doc, System.out);
+	}
 
+	/*
+		Root를 반환
+	 */
+	public Element getRoot() {
+		return doc.getRootElement();
+	}
+	
+	/*
+		자식 태그 중 해당하는 요소를 반환
+	 */
+	public Element navigate(Element element, String tag, String attr, String value) {
+		for(int i=0; i<element.getChildren().size(); i++) {
+			Element child = element.getChildren().get(i);
+			
+			if(child.getName().equals(tag)
+					&& child.getAttributeValue(attr).equals(value)) {
+				return child;
+			}
+		}
+		return null;
+	}
+	
+	public Element navigate(Element element, String tag) {
+		for(int i=0; i<element.getChildren().size(); i++) {
+			Element child = element.getChildren().get(i);
+			
+			if(child.getName().equals(tag)) {
+				return child;
+			}
+		}
+		return null;
+	}
+
+	/*
+		새로운 경로에 저장
+	 */
+	public void update(String newPath) throws Exception {
+		xout.output(doc, new FileOutputStream(newPath));
+	}
 }
--- JavaTest/src/com/aim/test/JdomTest.java
+++ JavaTest/src/com/aim/test/JdomTest.java
@@ -1,65 +1,79 @@
-package com.aim.test;
+package com.parser.jdom;
 
+import java.io.File;
 import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 public class JdomTest {
+	private static Document doc;
 	public static void main(String[] args) throws Exception {
-		String xml = "C:\\aim\\220311\\OPC_TagMap_Mill_0.75.xml";
-		String xmlNew = "C:\\aim\\220311\\OPC_TagMap_Mill_0.77.xml";
+		String xmlPath = "C:\\Aim\\OPC_TagMap_Mill_0.75.xml";
+		String newPath = "C:\\Aim\\Spring\\JavaTest\\OPC_TagMap_Mill_0.76.xml";
+
+		File file = new File(xmlPath);
 		
 		SAXBuilder builder = new SAXBuilder();
-		Document doc = builder.build(xml);
+		doc = builder.build(xmlPath);
 		
-		Element multiBlock = new Element("MultiBlock")
-									.setAttribute("Name", "MB_EVENT_REPLY_TURNTABLECOILIDREQUEST_APPEND")
-									.setAttribute("Action", "W");
-		Element block = new Element("Block")
-									.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND");
-		Element item = new Element("Item")
-									.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND")
-									.setAttribute("SyncValue", "true");
-		block.addContent(item);
-		multiBlock.addContent(block);
+//		Element multiBlock = new Element("MultiBlock")
+//									.setAttribute("Name", "MB_EVENT_REPLY_TURNTABLECOILIDREQUEST_APPEND")
+//									.setAttribute("Action", "W");
+//		Element block = new Element("Block")
+//									.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND");
+//		Element item = new Element("Item")
+//									.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND")
+//									.setAttribute("SyncValue", "true");
+//		block.addContent(item);
+//		multiBlock.addContent(block);
 		
 //		multiBlock.setAttribute("Name", "MB_EVENT_REPLY_TURNTABLECOILIDREQUEST_APPEND")
 //					.setAttribute("Action", "W");
 		
 		Element root = doc.getRootElement();
-		System.out.println(doc.getRootElement().getChildren().get(2));
-		List<Element> blockList = root.getChildren("Block");
-		System.out.println(blockList.size());
-		List<Element> itemList = root.getChildren("Item");
-		Element item2 = null;
-		for(Element e : blockList) {
-			if(e.getAttribute("Name").equals("CoilRollingDataSendData_M1")) {
-				itemList = e.getChildren("Item");
-				System.out.println(itemList.size());
-				break;
-			}
-		}
-
-		for(Element e : itemList) {
-			if(e.getAttribute("Name").equals("M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex")) {
-				item2 = e.setAttribute("Point", "10000");
-			}
-		}
-				for(Element e : rootList) {
-			for(int i=0; i<e.getChildren().size(); i++) {
-				if(e.getChildren().get(i).getName().equals("Block")) {
-					if(e.getChildren().get(i).getAttributeValue("Name").equals("CoilRollingDataSendData_M1")){
-						e.getChildren().get(i).setAttribute("Name", "Hi");
-					}
-				}
-			}
-		}
+//		System.out.println(root.getChildren().size());
 		
-		doc = new Document();
+		Element element = getChild(root, "BlockMap");
+		Element child = getChild(element, "Block", "Name", "CoilRollingDataSendData_M1");
+		Element child2 = getChild(child, "Item", "Name", "M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex");
+		child2.setAttribute("Point", "10000");
+		
+		
+//		System.out.println(doc.getRootElement().getChildren().get(2));
+//		List<Element> rootList = root.getChildren();
+//		
+//		for(Element e : rootList) {
+//			for(int i=0; i<e.getChildren().size(); i++) {
+//				if(e.getChildren().get(i).getName().equals("Block")) {
+//					if(e.getChildren().get(i).getAttributeValue("Name").equals("CoilRollingDataSendData_M1")){
+//						e.getChildren().get(i).setAttribute("Name", "Hi");
+//					}
+//				}
+//			}
+//		}
+		
+//		System.out.println(blockList.size());
+//		List<Element> itemList = root.getChildren("Item");
+//		Element item2 = null;
+//		for(Element e : blockList) {
+//			if(e.getAttribute("Name").equals("CoilRollingDataSendData_M1")) {
+//				itemList = e.getChildren("Item");
+//				System.out.println(itemList.size());
+//				break;
+//			}
+//		}
+//
+//		for(Element e : itemList) {
+//			if(e.getAttribute("Name").equals("M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex")) {
+//				item2 = e.setAttribute("Point", "10000");
+//			}
+//		}
+//		
+//		
+//		doc = new Document();
 //		doc.setRootElement(root);
 		XMLOutputter xout = new XMLOutputter();
 //		Format format = xout.getFormat();
@@ -69,6 +83,28 @@ public class JdomTest {
 //		xout.setFormat(format);
 //		xout.output(doc, new FileOutputStream(xmlNew));
 //		xout.output(doc, System.out);
-//		xout.output(root, System.out);
+		xout.output(doc, System.out);
+	}
+	public static Element getChild(Element element, String tag) {
+		for(int i=0; i<element.getChildren().size(); i++) {
+			if(element.getChildren().get(i).getName().equals(tag)) {
+				System.out.println(element.getChildren().get(i));
+				return element.getChildren().get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	public static Element getChild(Element e, String tag, String attr, String value) {
+		for(int i=0; i<e.getChildren().size(); i++) {
+			Element child = e.getChildren().get(i);
+			if(child.getName().equals(tag)
+					&& child.getAttributeValue(attr).equals(value)) {
+				return child;
+			}
+		}
+		
+		return null;
 	}
 }
