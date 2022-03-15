e7c919b0739fe212a396c63fc100e9b99d5e882a
YouMe <87955005+umingming@users.noreply.github.com>
2022-03-15 10:14:32 +0900

    Navigate simply

--- JavaTest/src/com/aim/test/Test.java
+++ JavaTest/src/com/aim/test/Test.java
@@ -1,31 +1,46 @@
-package com.aim.test;
+package com.parser.jdom2;
 
-import org.jsoup.nodes.Element;
+
+import org.jdom2.Element;
+import org.w3c.dom.NodeList;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		try {
-			String xml = "C:\\Aim\\Spring\\JavaTest\\OPC_TagMap_Mill_0.75.xml";
-			XmlParser parser = new XmlParser(xml);
+			// xml 파일 객체화 후 출력
+			String xmlPath = "C:\\Aim\\Spring\\JavaTest\\OPC_TagMap_Mill_0.75.xml";
+			JdomParser parser = new JdomParser(xmlPath);
+			parser.print();
 			
-//			parser.print();
+			// Item 태그 찾기
+			Element block = parser.navigate("Block", "Name", "CoilRollingDataSendData_M1");
+			Element item = parser.navigate(block, "Item", "Name", "M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex");
 			
-			String query = String.format("Block[Name=%s] Item[Name=%s]"
-										, "CoilRollingDataSendData_M1"
-										, "M1_MILL.EQ.CoilRollingDataSendData_M1_RefCurveIndex");
-			String attribute = "Point";
-			String value = "10000";
+			// Item의 Point 속성 변경
+			item.setAttribute("Point", "100000000");
 			
-			parser.modifyAttr(query, attribute, value);
+			// Trx 태그 찾기
+			Element trx = parser.navigate("Trx", "Name", "S7_MILL.EQ.Event_TurnTableCoilIDRequest");
 			
-			Element element = new Element("MultiBlock")
-									.attr("Name", "MB_EVENT_REPLY_TURNTABLECOILIDREQUEST_APPEND")
-									.attr("Action", "W");
-			element.append("<Block>");
-			System.out.println(element.toString());
+			// Trx에 MultiBlock 추가
+			Element multiBlock = new Element("MultiBlock")
+						.setAttribute("Name", "MB_EVENT_REPLY_TURNTABLECOILIDREQUEST_APPEND")
+						.setAttribute("Action", "W");
+			trx.addContent(multiBlock);
 			
+			block = new Element("Block")
+						.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND");
+			multiBlock.addContent(block);
+
+			item = new Element("Item")
+						.setAttribute("Name", "S7_MILL.EQ.Event_Reply_TurnTableCoilIDRequest_APPEND")
+						.setAttribute("SyncValue", "true");
+			block.addContent(item);
 			
+			// 새로운 경로에 저장
+			String newPath = "C:\\Aim\\OPC_TagMap_MILL_0.76_modify2.xml";
+			parser.update(newPath);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
