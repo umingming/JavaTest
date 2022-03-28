9b24de7eea8b939af4f613231cf60386eb019371
umingming <u_0112@naver.com>
2022-03-28 13:44:42 +0900

    Override run()

--- Socket/src/com/test/socket2/EchoServerThread.java
+++ Socket/src/com/test/socket2/EchoServerThread.java
@@ -1,5 +1,58 @@
 package com.test.socket2;
 
-public class EchoServerThread {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class EchoServerThread implements Runnable {
+	Socket socket;
+	
+	InputStream is;
+	ObjectInputStream ois;
+	
+	OutputStream os;
+	ObjectOutputStream oos;
+	
+	String msg;
+	
+	public EchoServerThread(Socket socket) {
+		this.socket = socket;
+		try {
+			System.out.println(socket.getInetAddress() + "로 부터 연결 요청을 받음.");
+			is = socket.getInputStream();
+			ois = new ObjectInputStream(is);
+			
+			os = socket.getOutputStream();
+			oos = new ObjectOutputStream(os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				msg = (String)ois.readObject();
+				System.out.println(socket.getInetAddress() + "의 메시지 : " + msg);
+				oos.writeObject(msg);
+				oos.flush();
+			}
+		} catch (Exception e) {
+			System.out.println("클라이언트가 강제 종료");
+		} finally {
+			try {
+				is.close();
+				os.close();
+				ois.close();
+				oos.close();
+				socket.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
