67bac9a0c373d66078a929e2cf16cd5599369de1
umingming <u_0112@naver.com>
2022-03-28 16:45:46 +0900

    Create main class to run thread

---
+++ Socket/src/com/test/thread/BeepPrintExample.java
@@ -0,0 +1,18 @@
+package com.test.thread;
+
+public class BeepPrintExample {
+
+	public static void main(String[] args) {
+		Thread thread = new BeepThread();
+		thread.start();
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(500);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
