16cb546d5a82abf345a41f83417a50cc0564be05
umingming <u_0112@naver.com>
2022-03-29 13:33:44 +0900

    Fix Echo msg

--- Socket/src/com/test/socket5/Client.java
+++ Socket/src/com/test/socket5/Client.java
@@ -1,5 +1,70 @@
 package com.test.socket5;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class Client {
+	/*
+		클라이언트
+		- 서버에 메시지를 보내고, 해당 메시지를 돌려 받을 것.
+		
+		1. IP와 PORT를 매개로 Socket 생성 후 접속 메시지 출력
+		2. BufferedReader 생성해 입력 값을 name 변수에 초기화함.
+		3. 서버에 데이터를 보내기 위해 OutputStream 생성
+		4. 데이터를 되돌려 받기 위해 InputStream 생성
+		5. name을 writeUTF의 매개로 서버에 전송함.
+		6. msg 변수 선언
+		7. while문 입력 값을 msg에 초기화 후,
+			msg가 null이 아닐 경우를 조건으로 반복; (msg = reader.readLine()) != null
+			> msg를 writeUTF의 매개로 서버에 전송함.
+			> flush 메소드로 OutputStream 확인
+			> if문 InputStream의 존재 데이터가 0이 아닌가?
+				> readUTF를 출력함.
+		8. 스트림과 소켓을 역순으로 닫기
+	 */
+	public static void main(String[] args) {
+		try {
+			Socket client = new Socket("localhost", 1234);
+			System.out.println("[서버 접속]");
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.println("[사용자 이름 입력]");
+			System.out.print("☞");
+			String name = reader.readLine();
+
+			OutputStream os = client.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+
+			InputStream is = client.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			
+			dos.writeUTF(name);
+			String msg;
+			
+			while((msg = reader.readLine()) != null) {
+				dos.writeUTF(msg);
+				
+				if(dis.available() != 0) {
+					System.out.println(dis.readUTF());
+				}
+				dos.flush();
+			}
+			
+			dis.close();
+			is.close();
+			os.close();
+			dos.close();
+			client.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Socket/src/com/test/socket5/Server.java
+++ Socket/src/com/test/socket5/Server.java
@@ -2,7 +2,6 @@ package com.test.socket5;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -11,13 +10,15 @@ import java.net.Socket;
 public class Server {
 	/*
 		에코 서버
+		- 클라이언트로부터 받은 메시지를 돌려줄 것.
+		
 		1. 1234 포트로 서버 소켓 생성 후 서버 생성 메시지 출력
 			> 생성 안 될 경우는?
 		2. accept() 메소드로 클라이언트의 접근 확인 후 소켓 생성
 		3. 클라이언트의 데이터를 받기 위한 InputStream 생성
 		4. 받은 데이터를 되돌려 주기 위해 OutputStream 생성
 		5. 사용자의 이름을 readUTF()로 저장함.
-		6. while문 stream 내 데이터가 0이 아닐 경우 반복; dis.avaliable() != 0
+		6. while문 InputStream 내 데이터가 0이 아닐 경우 반복; dis.avaliable() != 0
 			> readUTF()를 클라이언트의 이름과 변수 msg에 초기화함.
 			> msg를 서버에 출력함.
 			> OutputStream의 write 메소드로 msg를 전송함.
@@ -39,13 +40,23 @@ public class Server {
 			DataOutputStream dos = new DataOutputStream(os);
 			
 			String name = dis.readUTF();
+			System.out.printf("%s님이 접속했습니다.%n", name);
 			
-			while(dis.available() != 0) {
-				String msg = String.format("[%s] %s"
-										, name, dis.readUTF());
-				System.out.println(msg);
-				dos.writeUTF(msg);
-				dos.flush();
+			
+			while(true) {
+				if(dis.available() != 0) {
+					String msg = dis.readUTF();
+					
+					if(msg.equals("quit")) {
+						break;
+					}
+					
+					String echo = String.format("[%s] %s"
+							, name, msg);
+					System.out.println(echo);
+					dos.writeUTF(echo);
+					dos.flush();
+				}
 			}
 
 			os.close();
@@ -53,8 +64,10 @@ public class Server {
 			dis.close();
 			is.close();
 			client.close();
+
+			System.out.println("[서버 종료]");
 			
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
