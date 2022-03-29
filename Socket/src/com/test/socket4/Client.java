0267041a4c18e76bc33acb206c33ed7ee37ef6ea
umingming <u_0112@naver.com>
2022-03-29 10:28:50 +0900

    Add sending msg to server

--- Socket/src/com/test/socket4/Client.java
+++ Socket/src/com/test/socket4/Client.java
@@ -1,23 +1,37 @@
 package com.test.socket4;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
 		Socket client = new Socket("localhost", 1234);
 		System.out.println("서버 접속");
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("닉네임을 입력하세여");
+		String name = reader.readLine();
+
 		InputStream is = client.getInputStream();
 		DataInputStream dis = new DataInputStream(is);
+
+		OutputStream os = client.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
 		
-		String str = dis.readUTF();
-		String str2 = dis.readUTF();
+		dos.writeUTF(name);
+//		String str = dis.readUTF();
+//		String str2 = dis.readUTF();
 //		int number = dis.readInt();
-		System.out.println(str + ", " + str2);
-		
+//		System.out.println(str + ", " + str2);
+
+		dos.flush();
+		os.close();
+		dos.close();
 		dis.close();
 		is.close();
 		client.close();
--- Socket/src/com/test/socket4/Server.java
+++ Socket/src/com/test/socket4/Server.java
@@ -1,8 +1,10 @@
 package com.test.socket4;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -13,11 +15,11 @@ public class Server {
 		int port = 1234;
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("문자를 입력하세여");
-		String str = reader.readLine();
+//		System.out.println("문자를 입력하세여");
+//		String str = reader.readLine();
 //		int number = Integer.parseInt(reader.readLine());
-		System.out.println("문자를 입력하세여");
-		String str2 = reader.readLine();
+//		System.out.println("문자를 입력하세여");
+//		String str2 = reader.readLine();
 		ServerSocket server = new ServerSocket(port);
 		
 		System.out.println("서버 생성");
@@ -26,14 +28,22 @@ public class Server {
 			Socket client = server.accept();
 			System.out.println("사용자 접속 : " + client.getInetAddress());
 			
+			InputStream is = client.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			
 			OutputStream os = client.getOutputStream();
 			DataOutputStream dos = new DataOutputStream(os);
+			String name = dis.readUTF();
 			
-			dos.writeUTF(str);
-			dos.writeUTF(str2);
+			System.out.println(name);
+//			dos.writeUTF(str);
+//			dos.writeUTF(str2);
 //			dos.writeInt(number);
 //			dos.flush();
+			os.close();
 			dos.close();
+			dis.close();
+			is.close();
 			client.close();
 		}
 	}
