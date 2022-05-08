2c667284ad1e088c38f10b6bc5b0b324d2699372
umingming <u_0112@naver.com>
2022-05-08 10:40:42 +0900

    Test

--- JavaTest/src/com/aim/test/Temp.java
+++ JavaTest/src/com/aim/test/Temp.java
@@ -1,8 +1,10 @@
 package com.aim.test;
 
 public class Temp {
-	static int a;
+	public static int a = 100;
 	public static void main(String[] args) {
+		public int b = 10;
+		final double pi = 3.14;
 		System.out.println(a);
 	}
 
