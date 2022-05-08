2e4b2cdeac2432757747b4b45c9901c8f52cec89
umingming <u_0112@naver.com>
2022-05-08 11:00:47 +0900

    Test inerface example

--- JavaTest/src/com/aim/test/Temp.java
+++ JavaTest/src/com/aim/test/Temp.java
@@ -1,12 +1,40 @@
 package com.aim.test;
 
-public class Temp {
+public class Temp implements Inf2 {
 	public static void main(String[] args) {
 		int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
 		int sum = 0;
 		for(int number : numbers)
 				sum += number;
 		
+		Inf2 obj = new Temp();
+		obj.method2();
+		obj.method3();
+		
 	}
 
+	@Override
+	public void method1() {
+		System.out.println(a);
+		
+	}
+
+	@Override
+	public void method2() {
+		System.out.println(a);
+	}
+
+}
+
+interface Inf1 {
+	int a = 10;
+	public void method1();
+}
+
+interface Inf2 extends Inf1 {
+	int a = 20;
+	public void method2();
+	default void method3() {
+		System.out.println(a + Inf1.a);
+	}
 }
