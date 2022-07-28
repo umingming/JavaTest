3d09d8d2f63af8e5cf693e22ea1f78bf5af1de04
umingming <u_0112@naver.com>
2022-07-28 11:33:59 +0900

    Update method details

--- FileSorter/src/kr/co/aim/sorter/BasicSorter.java
+++ FileSorter/src/kr/co/aim/sorter/BasicSorter.java
@@ -15,39 +15,27 @@ public class BasicSorter {
 	 		> 파일 생성날짜에 해당하는 폴더로 이동
 	 */
 	public static void main(String[] args) throws IOException {
-		File dir = new File("C:\\Users\\user\\OneDrive - AIM System, Inc\\바탕 화면");
+		File dir = new File("C:\\Users\\user\\Downloads");
 		File[] fileList = dir.listFiles();
 		
 		for (File file : fileList) {
-			if(file.getName().indexOf('o') > 0) continue;
-			
-			BasicFileAttributes fileAttrs 
-				= Files.readAttributes(file.toPath(), BasicFileAttributes.class);
-
-			DateFormat format = new SimpleDateFormat("yyMMdd");
-			String fileDate = format.format(fileAttrs.lastModifiedTime().toMillis());
-			
-			String newPath = String.format("C:\\YouMe\\업무 파일\\%s\\%s\\%s"
-												, fileDate.substring(0, 4) 
-												, fileDate
-												, file.getName());
-			File newFile = new File(newPath);
-			newFile.mkdirs();
-			
-			System.out.println(newFile);
-			System.out.println(file.renameTo(newFile));
+			String newPath = String.format("%s\\%s"
+											, createDirectory(file)
+											, file.getName());
+			System.out.printf("%s%n -> %s ", file, newPath);
+			System.out.printf("[%s]%n", file.renameTo(new File(newPath)));
 		}
 	}
 	
 	/*
 		create directory; 해당 파일의 상위 폴더 생성
-		1. 파일 날짜 확인 후, 변수에 초기화
-		2. 날짜를 토대로 경로 선언 후, 파일 생성
+		1. 파일 날짜 변수 선언
+		2. 날짜를 토대로 경로에 해당하는 파일 생성
 		3. 해당 파일을 반환함.
 	 */
 	public static File createDirectory(File file) throws IOException {
 		String fileDate = getFileDate(file);
-		String path = String.format("C:\\YouMe\\업무 파일\\%s\\%s\\%s"
+		String path = String.format("C:\\YouMe\\업무 파일\\%s\\%s"
 											, fileDate.substring(0, 4) 
 											, fileDate);
 		File dir = new File(path);
